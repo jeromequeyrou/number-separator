19c958f919465a94b87c706c87969e53eb9bb413
jeromequeyrou <jerome.queyrou@gmail.com>
2017-08-18 18:02:32 +0200

    feat(src): the utilisator can choice the separator of decimal

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-function numberSeparator(number, separator) {
-  if (!separator) separator = " ";
+function numberSeparator(number, intSeparator, decimalSeparator) {
   var str = number.toString();
   var i = str.length - 1;
   var j = 0;
+  var float =
+    str.indexOf(".") != -1 && typeof decimalSeparator != "undefined" ? 1 : 0;
+  var separator = float
+    ? decimalSeparator
+    : typeof intSeparator != "undefined" ? intSeparator : " ";
   while (i >= 0) {
     if (str.charAt(i) == ".") {
       j = 0;
       i--;
+      separator = typeof intSeparator != "undefined" ? intSeparator : " ";
     } else if (j % 3 == 0 && j != 0 && str.charAt(i) != ".") {
       str = str.slice(0, i + 1) + separator + str.slice(i + 1);
       j = 0;
